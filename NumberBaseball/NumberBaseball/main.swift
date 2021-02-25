9ba474bef714b1226cfcbaed432cc978863cadc3
eunseo5355 <eunseo5355@naver.com>
2021-02-25 20:40:14 +0900

    [Step-2] 메뉴 입력받는 함수, 3개의 숫자 입력받는 함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -2,9 +2,29 @@
 //  NumberBaseball - main.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
-import Foundation
+var menuNum: Int?
+var threeNum: [Any]?
 
-print("Hello, World!")
+func inputMenuNum() {
+    print("1. 게임시작\n2. 게임종료")
+    print("원하는 기능을 선택해주세요 : ", terminator:"")
+    
+    guard let input = Int(readLine()!) else { return }
+    
+    menuNum = input
 
+    return
+}
+
+func inputNum() {
+    print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
+    print("중복 숫자는 허용하지 않습니다.")
+    
+    guard let inputThreeNum = readLine()?.split(separator: " ") else { return }
+    
+    threeNum = inputThreeNum
+    
+    return
+}
